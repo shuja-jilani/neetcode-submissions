2e205af5be8cd7e6e70d0da6e4aa05fd8ca86496
Shuja Husain Jilani <108534752+shuja-jilani@users.noreply.github.com>
2026-08-07 18:17:08 +0530

    Bulk sync: 6 submissions

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i< strs.size(); i++){
+            sb.append(strs.get(i)+ "#");
+            // sb.append("#");
+        }
+       return sb.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> strs = new ArrayList<>();
+        char[] arr = str.toCharArray();
+        int j = 0;
+        for(int i =0 ; i< arr.length; i++){
+            if(arr[i]=='#'){
+                strs.add(str.substring(j,i));
+                j = i+1; 
+            }
+        }
+        return strs;
+    }
+}
---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.java
@@ -0,0 +1,35 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i< strs.size(); i++){
+            sb.append(strs.get(i).length() + strs.get(i));
+        }
+       return sb.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> strs = new ArrayList<>();
+        char[] arr = str.toCharArray();
+        
+        int j = arr[0] - '0' + 1;
+        int i =1;
+        while(j<=str.length()+1){
+            strs.add(str.substring(i,j));
+            i = j+1;
+            if(j>= str.length()){
+                break;
+            }
+            j = j + (arr[j] - '0') + 1;
+        }
+        
+//5hello6worldi4word 17 13+4+1
+        // for(int i =0 ; i< arr.length; i++){
+        //     if(arr[i]=='^'){
+        //         strs.add(str.substring(j,i));
+        //         j = i+1; 
+        //     }
+        // }
+        return strs;
+    }
+}
