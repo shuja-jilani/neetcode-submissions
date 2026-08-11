c9426dfeae925b932cf60b13e939e272b91927ee
Shuja Husain Jilani <108534752+shuja-jilani@users.noreply.github.com>
2026-08-11 17:52:18 +0530

    Add: trapping-rain-water - submission-2

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int trap(int[] height) {
+        // we need to know the local heights around the current index and pick the minimum of those two to know the qty of water trapped at this current index.
+        int total = 0;
+        int[] l = new int[height.length];
+        int[] r = new int[height.length];
+        int currentMaxLeft = height[0];
+        int currentMaxRight = height[height.length-1]; 
+        for(int i =1 ; i < height.length; i++){
+           currentMaxLeft = Math.max(currentMaxLeft, height[i]);
+           l[i] = currentMaxLeft;
+        }
+        for(int i = height.length-2; i>=0 ; i--){
+            currentMaxRight = Math.max(currentMaxRight, height[i]);
+            r[i] = currentMaxRight;
+        }
+        for(int i = 0 ; i<height.length; i++){
+            int tempWater = Math.min(l[i], r[i]) - height[i];
+            if(tempWater > 0 ){
+                total += tempWater;
+            }
+        }
+        return total;
+    }
+}
