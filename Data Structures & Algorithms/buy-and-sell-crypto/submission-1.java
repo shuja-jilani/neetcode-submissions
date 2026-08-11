fffb71a14aeab25fb4061ee163f617e0a70f3854
Shuja Husain Jilani <108534752+shuja-jilani@users.noreply.github.com>
2026-08-11 16:28:54 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int min = prices[0];
+        for(int i = 1; i< prices.length; i++){
+            min = Math.min(min,prices[i]);
+            if(prices[i]>min){
+                int tempProfit = prices[i]-min;
+                profit = Math.max(profit, tempProfit);
+            }
+        }
+        return profit;
+    }
+}
