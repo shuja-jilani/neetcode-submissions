5b3b45f216dd2f4530475cac9d7a3add545e3ea8
Shuja Husain Jilani <108534752+shuja-jilani@users.noreply.github.com>
2026-08-25 15:08:32 +0530

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int n = rows * cols; //totalelements
+        int l = 0;
+        int r = n-1; 
+        while(l<=r){
+            int mid = l + (r-l)/2; 
+            int row = mid / cols; //basic idea h , divide isliye kr rhe h qki agar second row h to km se km ek row me jitne cols h wo paar ho chuke h to divide kiye to answer 1 hi ata yani second row.
+            int col = mid % cols;
+            if(matrix[row][col] == target){
+                return true;
+            }
+            else if(matrix[row][col] < target){
+                l = mid + 1;
+            }
+            else{
+                r = mid - 1; 
+            }
+        }
+        return false;
+    }
+}
